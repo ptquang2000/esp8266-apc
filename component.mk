c83a4c7ccd7b01f421412a6e1f2ea35fa2063fa2
quang phan <ptquang2000@gmail.com>
2023-05-02 07:40:19 +0700

    update prj structure

--- component.mk
+++
@@ -1,7 +0,0 @@
-#
-# Component Makefile
-#
-COMPONENT_ADD_INCLUDEDIRS := apc/include
-COMPONENT_SRCDIRS := apc
-COMPONENT_SUBMODULES := apc
-COMPONENT_OBJS := apc/apc.o apc/vl53l1_platform.o apc/VL53L1X_api.o apc/VL53L1X_calibration.o
